01b606842ef54c1ff89b4494ff2aebe3997bacd5
Dima-salang <luisgabrielle1026@gmail.com>
2026-01-10 14:07:39 +0800

    del: most of the cmd logic and replaced it with starting the model

--- cmd/start.go
+++ cmd/start.go
@@ -5,7 +5,7 @@ import (
 	"time"
 
 	"github.com/Dima-salang/pomolite/timer"
-	"github.com/eiannone/keyboard"
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/spf13/cobra"
 )
 
@@ -28,7 +28,6 @@ var startCmd = &cobra.Command{
 		if !timer.CheckInput(minutes, breakMinutes) {
 			return
 		}
-		var storage timer.Storage
 		storage, err := timer.NewSQLiteStorage("./pomodoro.db")
 
 		if err != nil {
@@ -37,20 +36,15 @@ var startCmd = &cobra.Command{
 		}
 		defer storage.Close()
 
-		totalWorkDuration := time.Duration(minutes)*time.Minute
+		totalWorkDuration := time.Duration(minutes) * time.Minute
 		totalBreakDuration := time.Duration(breakMinutes) * time.Minute
 
-		pt := timer.NewPomodoroTimer(totalWorkDuration, totalBreakDuration, label)
-		go timer.ListenForCommands(pt.ControlChan)
+		m := timer.NewPomoModel(totalWorkDuration, totalBreakDuration, label, storage)
+		p := tea.NewProgram(m)
 
-		defer keyboard.Close()
-		for {
-			ok := pt.Start()
-			if !ok {
-				pt.EndTime = time.Now()
-				storage.SaveTimerData(pt.WorkLabel, pt.StartTime, pt.EndTime)
-				return
-			}
+		if _, err := p.Run(); err != nil {
+			fmt.Printf("Alas, there's been an error: %v", err)
+			return
 		}
 	},
 }
